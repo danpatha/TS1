bba507b8d1b378a2622677618f274ee4a0148630
Daniel Pathammavong <patha037@umn.edu>
2023-07-27 15:23:29 -0500

    add temps and weather description

--- starter-files/src/components/Forecast.tsx
+++ starter-files/src/components/Forecast.tsx
@@ -4,7 +4,11 @@ type Props ={
  data:forecastType
 }
 
-
+const Degree = ({temp}:{temp:number}): JSX.Element => (
+    <span>
+        {temp}<sup>o</sup>
+    </span>
+)
 
 const Forecast = ({data}: Props): JSX.Element => {
     const today = data.list[0]
@@ -19,13 +23,25 @@ const Forecast = ({data}: Props): JSX.Element => {
            <h2 className='text-2x1 font-black'>{data.name}
            <span className="font-thin">, {data.country}</span>
            </h2>
-           <h1 className="text-4x-1 font-extrabold">{Math.round(today.main.temp)}
+           <h1 className="text-4x-1 font-extrabold">
+
+          <Degree temp={Math.round(today.main.temp)}/>
            </h1>
+           <p className="text-sm">
+           {today.weather[0].main} {''}
+           {today.weather[0].description} 
+
+           </p>
+           <p className="text-sm">
+            H:<Degree temp={Math.ceil(today.main.temp_max)}/> {''}
+            L:<Degree temp={Math.floor(today.main.temp_min)}/>
+
+           </p>
            </section>
            </div>
         </div>
 
     )
-    //Start at 1:20 after.
+    //Start at 1:22 after.
 }
 export default Forecast
